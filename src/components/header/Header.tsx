5e46061807af3c4bd96cd0f97eeca9666c1d0c67
Wasim9Daoud <wasimdaoud05@gmail.com>
2024-09-20 20:24:00 +0300

    add header component

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,21 @@
+import Logo from "./logo/Logo";
+import RightBar from "./Rightbar/RightBar";
+import style from "@/components/header/header.module.css";
+import NavBar from "./navBar/NavBar";
+import { cookies } from "next/headers";
+import { verifyTokenForPages } from "@/utils/verifyToken";
+
+const Header = () => {
+  const token = cookies().get('jwtToken')?.value || "";
+  const payload = verifyTokenForPages( token );
+  return (
+    <div className="h-[65px] flex sticky top-0 items-center justify-between py-[0] px-[40px] border-b-[2px] border-white bg-blue-400 transition-all duration-1000" >
+      <Logo />
+      <NavBar isAdmin={payload?.isadmin || false}/>
+      <RightBar />
+    </div>
+  ); 
+};
+
+export default Header;
+
