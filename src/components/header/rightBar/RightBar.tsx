af1149d23ced830fce67fe4ba6f108210e0dd057
Wasim9Daoud <wasimdaoud05@gmail.com>
2024-09-20 20:27:26 +0300

    add rightBar  component

---
+++ src/components/header/rightBar/RightBar.tsx
@@ -0,0 +1,34 @@
+import style from "@/components/header/header.module.css";
+import Link from "next/link";
+import { cookies } from "next/headers";
+import { verifyTokenForPages } from "@/utils/verifyToken";
+import Logout from "./Logout";
+
+const RightBar = () => {
+  const token = cookies().get("jwtToken")?.value || "";
+  const payload = verifyTokenForPages(token);
+
+  return (
+    <div className="flex gap-2">
+      {token ? (
+        <>
+          <strong className="text-purple-700  md:text-xl capitalize">
+            {payload?.username}
+          </strong>
+          <Logout />
+        </>
+      ) : (
+        <>
+          <Link href="/login" className={style.btn}>
+            Login
+          </Link>
+          <Link href="/register" className={style.btn}>
+            Register
+          </Link>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default RightBar;
