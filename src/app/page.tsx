3a1e886ebc8943af4d672899c5c1b3de9cf76230
Wasim9Daoud <wasimdaoud05@gmail.com>
2024-09-24 14:10:16 +0300

    add Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
 
-export default function Home() {
+import Hero from "@/components/Home/Hero"
+import WebHostingPlan from "@/components/Home/WebHostingPlan"
+
+const page = () => {
   return (
-    <div className="">
-    </div>
-  );
+    <section className="bg-purple-50">
+      <Hero />
+      <h2 className="text-center mt-10 text-3xl text-gray-500 font-bold">
+        Choose Your Web Hosting Plan
+      </h2>
+      <div className=" container m-auto flex justify-center items-center my-7 flex-wrap md:gap-7 ">
+        <WebHostingPlan />
+        <WebHostingPlan />
+        <WebHostingPlan />
+      </div>
+    </section>
+  )
 }
+
+export default page
+
