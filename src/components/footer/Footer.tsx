691c2fc216788d1b324fea770a2022c2730a24fe
Wasim9Daoud <wasimdaoud05@gmail.com>
2024-10-01 19:40:44 +0300

    add footer component

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+      <section className="w-full h-[50px] flex justify-center items-center bg-gray-700">
+        <p className="text-lg text-gray-300"> Copyright 2024 Cloud Hosting </p>
+      </section>
+    );
+  };
+  
+  export default Footer;
+  
