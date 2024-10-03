ec45b3937001ea99769461490c4dc529605b859b
Wasim9Daoud <wasimdaoud05@gmail.com>
2024-10-03 21:30:02 +0300

    add about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,16 @@
+
+const about = () => {
+    return (
+      <section className="aboutPage-Height m-auto">
+        <h1 className="text-3xl font-bold text-gray-800 p-5">
+          About This App
+        </h1>
+        <p className="px-5 text-gray-600 text-xl">
+          the bes web hosting solution for your online success
+        </p>
+      </section>
+    )
+  }
+  
+  export default about
+  
